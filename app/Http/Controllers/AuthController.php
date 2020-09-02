1ac57a7ee389ff226bc6a11d776c73d99bc70f6c
Ilham Cahyahadi Pamungkas <ilham.cahyahadi.tif17@polban.ac.id>
2020-09-02 23:36:39 +0700

    Penambahan waktu expire token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -110,7 +110,7 @@ class AuthController extends BaseController
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 60 * 24 * 5
         ]);
     }
 }
