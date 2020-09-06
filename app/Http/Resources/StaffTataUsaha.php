1ba6480ba369a9ab2d5938ddbc2486b86e5d5af0
Ilham Cahyahadi Pamungkas <ilham.cahyahadi.tif17@polban.ac.id>
2020-09-07 00:48:03 +0700

    Perbaikan Response API Staff Tata Usaha

--- app/Http/Resources/StaffTataUsaha.php
+++ app/Http/Resources/StaffTataUsaha.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\UserCollection;
+use App\Http\Resources\User as UserResource;
 use App\User;
 class StaffTataUsaha extends JsonResource
 {
@@ -18,8 +18,13 @@ class StaffTataUsaha extends JsonResource
         return [
             'kd_staff' => $this->kd_staff,
             'nama_staff' => $this->nama_staff,
-            'id_user' => new UserCollection(User::find($this->user)),
-            'foto_staff' => $this->foto_staff
+            'id_user' => $this->id_user,
+            'foto_staff' => $this->foto_staff,
+            $this->mergeWhen($this->user()->exists() && $this->user->count()!=0, function() {
+                return [
+                    'user' => new UserResource($this->user)
+                ];
+            })
         ];
     }
 }
