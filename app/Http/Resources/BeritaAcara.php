b0f1697a09ecf1a68ff4cec2c50f16315a8aa718
Ilham Cahyahadi Pamungkas <ilham.cahyahadi.tif17@polban.ac.id>
2020-09-14 19:46:52 +0700

    perbaikan response berita acara

--- app/Http/Resources/BeritaAcara.php
+++ app/Http/Resources/BeritaAcara.php
@@ -16,16 +16,19 @@ class BeritaAcara extends JsonResource
     {
         return [
             'kd_berita_acara' => $this->kd_berita_acara,
-            'kd_jadwal' => [
-                'jadwal' => $this->jadwal
-            ],
+            'kd_jadwal' => $this->kd_jadwal,
             'desk_perkuliahan' => $this->desk_perkuliahan,
             'desk_penugasan' => $this->desk_penugasan,
             'tgl_pertemuan' => $this->tgl_pertemuan,
             'mhs_hadir' => $this->mhs_hadir,
             'mhs_tidak_hadir' => $this->mhs_tidak_hadir,
             'jam_presensi_dibuka' => $this->jam_presensi_dibuka,
-            'jam_presensi_ditutup' => $this->jam_presensi_ditutup
+            'jam_presensi_ditutup' => $this->jam_presensi_ditutup,
+            $this->mergeWhen($this->jadwal()->exists() && $this->jadwal->count()!=0, function() {
+                return [
+                    'jadwal' => $this->jadwal
+                ];
+            })
         ];
     }
 }
