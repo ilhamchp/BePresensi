2001f209c827b204dfd6c40cdaab98e795a3d94b
Ilham Cahyahadi Pamungkas <ilham.cahyahadi.tif17@polban.ac.id>
2020-09-19 01:17:00 +0700

    Perbaikan Response Surat Izin

--- app/Http/Resources/SuratIzin.php
+++ app/Http/Resources/SuratIzin.php
@@ -23,16 +23,32 @@ class SuratIzin extends JsonResource
     {
         return [
             'kd_surat_izin' => $this->kd_surat_izin,
-            'kd_jenis_izin' => new StatusPresensiCollection(StatusPresensi::find($this->jenisIzin)),
-            'nim' => new MahasiswaCollection(Mahasiswa::find($this->mahasiswa)),
+            'kd_jenis_izin' => $this->kd_jenis_izin,
+            'nim' => $this->nim,
             'tgl_mulai' => (string) $this->tgl_mulai,
             'tgl_selesai' => (string) $this->tgl_selesai,
             'jam_mulai' => (string) $this->jam_mulai,
             'jam_selesai' => (string) $this->jam_selesai,
-            'status_surat' => new StatusSuratCollection(StatusSurat::find($this->statusSurat)),
+            'status_surat' => $this->status_surat,
             'catatan_surat' => $this->catatan_surat,
             'catatan_wali_dosen' => $this->catatan_wali_dosen,
-            'foto_surat' => $this->foto_surat
+            'foto_surat' => $this->foto_surat,
+            $this->mergeWhen($this->jenisIzin()->exists() && $this->jenisIzin->count()!=0, function() {
+                return [
+                    'jenis_izin' => $this->jenisIzin
+                ];
+            }),
+            $this->mergeWhen($this->mahasiswa()->exists() && $this->mahasiswa->count()!=0, function() {
+                return [
+                    'mahasiswa' => $this->mahasiswa
+                ];
+            }),
+            $this->mergeWhen($this->statusSurat()->exists() && $this->statusSurat->count()!=0, function() {
+                return [
+                    'status_surat' => $this->statusSurat
+                ];
+            }),
+            
         ];
     }
 }
