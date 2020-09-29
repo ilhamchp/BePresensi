27f8db53490ebc6b7664ae56cc4ba5b072f213d2
Ilham Cahyahadi Pamungkas <ilham.cahyahadi.tif17@polban.ac.id>
2020-09-29 20:42:45 +0700

    Sesi Seeder for Development

--- database/seeds/SesiSeeder.php
+++ database/seeds/SesiSeeder.php
@@ -82,5 +82,89 @@ class SesiSeeder extends Seeder
             'jam_mulai' => '17:00:01',
             'jam_berakhir' => '17:50:00'
         ]);
+
+        Sesi::create([
+            'kd_sesi' => '13',
+            'jam_mulai' => '17:50:01',
+            'jam_berakhir' => '18:40:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '14',
+            'jam_mulai' => '18:40:01',
+            'jam_berakhir' => '19:30:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '15',
+            'jam_mulai' => '19:30:01',
+            'jam_berakhir' => '20:20:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '16',
+            'jam_mulai' => '20:20:01',
+            'jam_berakhir' => '21:10:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '17',
+            'jam_mulai' => '21:10:01',
+            'jam_berakhir' => '22:00:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '18',
+            'jam_mulai' => '22:00:01',
+            'jam_berakhir' => '22:50:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '19',
+            'jam_mulai' => '22:50:01',
+            'jam_berakhir' => '23:40:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '20',
+            'jam_mulai' => '23:40:01',
+            'jam_berakhir' => '23:59:59'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '21',
+            'jam_mulai' => '00:00:01',
+            'jam_berakhir' => '00:50:00'
+        ]);
+
+
+        Sesi::create([
+            'kd_sesi' => '22',
+            'jam_mulai' => '00:50:01',
+            'jam_berakhir' => '01:40:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '23',
+            'jam_mulai' => '01:40:01',
+            'jam_berakhir' => '02:30:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '24',
+            'jam_mulai' => '02:30:01',
+            'jam_berakhir' => '03:20:00'
+        ]);
+
+        Sesi::create([
+            'kd_sesi' => '25',
+            'jam_mulai' => '03:20:01',
+            'jam_berakhir' => '04:10:00'
+        ]);
+        Sesi::create([
+            'kd_sesi' => '26',
+            'jam_mulai' => '04:10:01',
+            'jam_berakhir' => '05:00:00'
+        ]);
     }
 }
